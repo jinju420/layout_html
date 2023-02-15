4c4db79a5ad64ae179a0c96e80f2f2d299fa7a86
jinju420 <ljjshlove@nate.com>
2023-02-15 22:52:32 +0900

    서브페페이지수정

--- js/flickr.js
+++ js/flickr.js
@@ -1,18 +1,17 @@
-
 const base = 'https://www.flickr.com/services/rest/?';
 // const method = 'flickr.interestingness.getList';
 const method = 'flickr.favorites.getList';
 const method1 = 'flickr.photos.search';
 const flickr_key = '8dfeab6f923483f4b3694e700652632a';
-const user_id = '195427004@N07';//즐겨찾기이미지
+const user_id = '195427004@N07'; //즐겨찾기이미지
 const per_page = 12;
 const galCont = document.querySelector('.gallery');
 const gal = galCont.querySelector('#gallery');
-const frame = gal.querySelector('#list');
-const loading = gal.querySelector('.loading');
-const input = gal.querySelector('#search');
-const btn = gal.querySelector('.btn');
-const youtube = galCont.querySelector('.vids')
+const frame = galCont.querySelector('#list');
+const loading = galCont.querySelector('.loading');
+const input = galCont.querySelector('#search');
+const btn = galCont.querySelector('.btn');
+// const youtube = document.querySelector('.vids');
 const url1 = `${base}method=${method}&api_key=${flickr_key}&per_page=${per_page}&format=json&nojsoncallback=1&user_id=${user_id}`;
 const url2 = `${base}method=${method1}&api_key=${flickr_key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=인테리어&privacy_filter=1`;
 
@@ -21,47 +20,46 @@ callDate(url2);
 
 //검색어 입력 후 버튼
 btn.addEventListener('click', () => {
-    let txt = input.value;
-    txt = txt.trim();//공백을 없애는 함수
-    const url = `${base}method=${method1}&api_key=${flickr_key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=${txt}&privacy_filter=1`;
+	let txt = input.value;
+	txt = txt.trim(); //공백을 없애는 함수
+	const url = `${base}method=${method1}&api_key=${flickr_key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=${txt}&privacy_filter=1`;
 
-    if (txt != '') {
-        callDate(url);
-    } else {
-        alert('검색어를 입력하세요');
-    }
-})
+	if (txt != '') {
+		callDate(url);
+	} else {
+		alert('검색어를 입력하세요');
+	}
+});
 //엔터키
-input.addEventListener('keypress', e => {
-    if (e.keyCode == 13) {
-        let txt = input.value;
-        txt = txt.trim();
-        const url = `${base}method=${method1}&api_key=${flickr_key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=${txt}&privacy_filter=1`;
+input.addEventListener('keypress', (e) => {
+	if (e.keyCode == 13) {
+		let txt = input.value;
+		txt = txt.trim();
+		const url = `${base}method=${method1}&api_key=${flickr_key}&per_page=${per_page}&format=json&nojsoncallback=1&tags=${txt}&privacy_filter=1`;
 
-        if (txt != '') {
-            callDate(url);
-        } else {
-            alert('검색어를 입력하세요');
-        }
+		if (txt != '') {
+			callDate(url);
+		} else {
+			alert('검색어를 입력하세요');
+		}
+	}
+});
 
-    }
-})
+frame.addEventListener('click', (e) => {
+	e.preventDefault();
+	//클릭한 대상이 #list이면 return하도록
+	if (e.target == frame) return;
+	//썸네일을 클릭해야지만 볼수있도록
+	let target = e.target.closest('.imgs').querySelector('.thumb');
 
-frame.addEventListener('click', e => {
-    e.preventDefault();
-    //클릭한 대상이 #list이면 return하도록
-    if (e.target == frame) return;
-    //썸네일을 클릭해야지만 볼수있도록
-    let target = e.target.closest('.imgs').querySelector('.thumb');
-
-    //사용자가 클릭한 대상이 썸네일인지
-    if (e.target == target) {
-        //let imgSrc = target.parentElement.getAttribute('href'); //thumb의 부모인 a태그를 찾는 방법
-        let imgSrc = target.closest('a').getAttribute('href'); //thumb에서 가장 가까이에 있는 a태그를 찾는 방법
-        let pop = document.createElement('aside');
-        pop.classList.add('pop');
-        document.body.classList.add('hidden');
-        let pops = `
+	//사용자가 클릭한 대상이 썸네일인지
+	if (e.target == target) {
+		//let imgSrc = target.parentElement.getAttribute('href'); //thumb의 부모인 a태그를 찾는 방법
+		let imgSrc = target.closest('a').getAttribute('href'); //thumb에서 가장 가까이에 있는 a태그를 찾는 방법
+		let pop = document.createElement('aside');
+		pop.classList.add('pop');
+		document.body.classList.add('hidden');
+		let pops = `
         <div class='imgCont'>
             <img src="${imgSrc}">
             <span class="close">
@@ -69,66 +67,63 @@ frame.addEventListener('click', e => {
             </span>
         </div>
         `;
-        pop.innerHTML = pops;
-        gal.append(pop);
-    } else {
-        return;
-    }
+		pop.innerHTML = pops;
+		gal.append(pop);
+	} else {
+		return;
+	}
 });
 
-gal.addEventListener('click', (e) => {
-    //pop이라는 클래스를 추적한다
-    let pop = gal.querySelector('.pop');
-    if (pop != null) {
-        //팝이 존재하는지
-        let close = pop.querySelector('.close> i');
-        if (e.target == close) {
-            pop.remove();
-            document.body.classList.remove('hidden');
-        }
-    }
+galCont.addEventListener('click', (e) => {
+	//pop이라는 클래스를 추적한다
+	let pop = gal.querySelector('.pop');
+	if (pop != null) {
+		//팝이 존재하는지
+		let close = pop.querySelector('.close> i');
+		if (e.target == close) {
+			pop.remove();
+			document.body.classList.remove('hidden');
+		}
+	}
 });
 
-//리사이즈시 
+//리사이즈시
 window.addEventListener('resize', () => {
-    let pop = gal.querySelector('.pop');
-    if (pop) document.body.classList.add('hidden');
-
+	let pop = gal.querySelector('.pop');
+	if (pop) document.body.classList.add('hidden');
 });
 
 //flickr
 function callDate(url1) {
-    frame.innerHTML = '';
-    loading.classList.remove('off');
-    frame.classList.remove('on');
-    fetch(url1)
-        .then((data) => {
-            const result = data.json();
-            return result;
-        })
-        .then((json) => {
-            const items = json.photos.photo;
+	frame.innerHTML = '';
+	loading.classList.remove('off');
+	frame.classList.remove('on');
+	fetch(url1)
+		.then((data) => {
+			const result = data.json();
+			return result;
+		})
+		.then((json) => {
+			const items = json.photos.photo;
 
-            if (items.length > 0) {
-                createImgs(items);
-                Loading();
-            } else {
-                alert('검색하신 이미지의 데이터가 없습니다');
-            }
-
-        })
+			if (items.length > 0) {
+				createImgs(items);
+				Loading();
+			} else {
+				alert('검색하신 이미지의 데이터가 없습니다');
+			}
+		});
 }
-
 
 //반복문
 function createImgs(items) {
-    htmls = '';
+	htmls = '';
 
-    items.map((item) => {
-        const imgSrcBig = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
-        const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
+	items.map((item) => {
+		const imgSrcBig = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
+		const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`;
 
-        htmls += `
+		htmls += `
             <li class="imgs">
                 <div>
                     <a href=${imgSrcBig}>
@@ -137,36 +132,36 @@ function createImgs(items) {
                     </div>
                     </li>
                     `;
-                })
-                frame.innerHTML = htmls;
-            }
-            // <p>${item.title}</p>
+	});
+	frame.innerHTML = htmls;
+}
+// <p>${item.title}</p>
 
 //로딩
 function Loading() {
-    const imgs = frame.querySelectorAll('img');
-    const len = imgs.length;
+	const imgs = frame.querySelectorAll('img');
+	const len = imgs.length;
 
-    let count = 0;
-    for (const el of imgs) {
-        el.addEventListener('load', () => {
-            count++;
-            if (count == len) isoLayout();
-        })
+	let count = 0;
+	for (const el of imgs) {
+		el.addEventListener('load', () => {
+			count++;
+			if (count == len) isoLayout();
+		});
 
-        el.addEventListener('error', () => {
-            e.currentTarget.closest('.imgs').querySelector('img').setAttribute('src', 'img/k1.jpg');
-        })
-    }
+		el.addEventListener('error', () => {
+			e.currentTarget.closest('.imgs').querySelector('img').setAttribute('src', 'img/k1.jpg');
+		});
+	}
 }
 
 //istope함수
 function isoLayout() {
-    loading.classList.add('off');
-    frame.classList.add('on');
-    new Isotope('#list', {
-        itemSelection: '.imgs',
-        columnWidth: '.imgs',
-        transitionDuration: '0.5s',
-    })
+	loading.classList.add('off');
+	frame.classList.add('on');
+	new Isotope('#list', {
+		itemSelection: '.imgs',
+		columnWidth: '.imgs',
+		transitionDuration: '0.5s',
+	});
 }
--- js/youtube.js
+++ js/youtube.js
@@ -5,42 +5,41 @@ const playlistId = 'PLY0voYdGZtAgPCRyH8libB1_TbuzvCR_a';
 const num = 6;
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
 
-const gallery = document.querySelector('.gallery');
-const galleryImg = gallery.querySelectorAll('.second .pic');
-const colorBtn = gallery.querySelectorAll('.colorBtn a');
+const gallery = document.querySelector('.youtube');
+// const galleryImg = gallery.querySelectorAll('.second .pic');
+// const colorBtn = gallery.querySelectorAll('.colorBtn a');
 
-const tab_title = gallery.querySelectorAll('h2');
-const tab_cont = gallery.querySelectorAll('.cont');
+// const tab_title = gallery.querySelectorAll('h2');
+// const tab_cont = gallery.querySelectorAll('.cont');
 
-tab_title.forEach((el, idx) => {
-    el.addEventListener('click', e => {
-        e.preventDefault();
+// tab_title.forEach((el, idx) => {
+// 	el.addEventListener('click', (e) => {
+// 		e.preventDefault();
 
-        activation(tab_title, idx);
-        activation(tab_cont, idx);
-    });
-});
+// 		activation(tab_title, idx);
+// 		activation(tab_cont, idx);
+// 	});
+// });
 
 //youtube
 fetch(url)
-    .then((data) => {
-        return data.json();
-    })
-    .then((json) => {
-        let items = json.items;
-        let result = '';
+	.then((data) => {
+		return data.json();
+	})
+	.then((json) => {
+		let items = json.items;
+		let result = '';
 
-        items.map((el) => {
+		items.map((el) => {
+			let title = el.snippet.title;
+			let des = el.snippet.description;
+			let date = el.snippet.publishedAt;
 
-            let title = el.snippet.title;
-            let des = el.snippet.description;
-            let date = el.snippet.publishedAt;
+			if (title.length > 30) title = title.substr(0, 15) + '...';
+			if (des.length > 100) des = des.substr(0, 30) + '...';
+			date = date.split('T')[0];
 
-            if (title.length > 30) title = title.substr(0, 15) + '...';
-            if (des.length > 100) des = des.substr(0, 30) + '...';
-            date = date.split('T')[0];
-
-            result += `
+			result += `
             <article>
                 <a href="${el.snippet.resourceId.videoId}" class="pic">
                     <img src="${el.snippet.thumbnails.standard.url}" />
@@ -52,20 +51,20 @@ fetch(url)
                 </div>
             </article>
             `;
-        })
-        vids.innerHTML = result;
-    });
+		});
+		vids.innerHTML = result;
+	});
 
 //popup생성
-vids.addEventListener('click', e => {
-    e.preventDefault();
+vids.addEventListener('click', (e) => {
+	e.preventDefault();
 
-    if (!e.target.closest('a')) return;
-    const vidId = e.target.closest('a').getAttribute('href');
-    const videoPop = document.createElement('figure');
-    videoPop.classList.add('popup');
+	if (!e.target.closest('a')) return;
+	const vidId = e.target.closest('a').getAttribute('href');
+	const videoPop = document.createElement('figure');
+	videoPop.classList.add('popup');
 
-    videoPop.innerHTML = `
+	videoPop.innerHTML = `
     <div class='iframe'>
         <iframe src='https://www.youtube.com/embed/${vidId}' frameborder='0' width='100%'
         height='100%' allowfullscreen></iframe>
@@ -75,35 +74,34 @@ vids.addEventListener('click', e => {
     </div>
     `;
 
-    vids.append(videoPop);
-    document.body.classList.add('hidden');
+	vids.append(videoPop);
+	document.body.classList.add('hidden');
 });
 
 //popup닫기
-vids.addEventListener('click', e => {
-    const pop = vids.querySelector('.popup');
-    if (pop) {
-        const close = pop.querySelector('.closeBtn > i');
-        if (e.target == close) {
-            pop.remove();
-            document.body.classList.remove('hidden');
-        }
-    }
+vids.addEventListener('click', (e) => {
+	const pop = vids.querySelector('.popup');
+	if (pop) {
+		const close = pop.querySelector('.closeBtn > i');
+		if (e.target == close) {
+			pop.remove();
+			document.body.classList.remove('hidden');
+		}
+	}
 });
 
-
-//리사이즈시 
-window.addEventListener('resize', e => {
-    const pop = vids.querySelector('.popup');
-    if (pop) document.body.classList.add('hidden');
+//리사이즈시
+window.addEventListener('resize', (e) => {
+	const pop = vids.querySelector('.popup');
+	if (pop) document.body.classList.add('hidden');
 });
 
 function isoLayout() {
-    loading.classList.add('off');
-    frame.classList.add('on');
-    new Isotope('#list', {
-        itemSelection: '.imgs',
-        columnWidth: '.imgs',
-        transitionDuration: '0.5s',
-    })
+	loading.classList.add('off');
+	frame.classList.add('on');
+	new Isotope('#list', {
+		itemSelection: '.imgs',
+		columnWidth: '.imgs',
+		transitionDuration: '0.5s',
+	});
 }
